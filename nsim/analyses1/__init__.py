80b422f36c19fd6cee4c31e12c088b3b3d9c1212
Matthew Aburn <mattja6@gmail.com>
2015-08-16 16:57:02 +1000

    update pyeeg.py to upstream version 2015-07-17

--- nsim/analyses1/__init__.py
+++ nsim/analyses1/__init__.py
@@ -8,9 +8,9 @@ from .freq import (psd, lowpass, highpass, bandpass, notch, hilbert,
 
 from .epochs import (variability_fp, epochs, epochs_distributed, epochs_joint)
 
-from .pyeeg import (hurst, embed_seq, in_range, bin_power, first_order_diff, 
-                    pfd, hfd, hjorth, spectral_entropy, svd_entropy, 
-                    fisher_info, ap_entropy, samp_entropy, dfa)
+from .pyeeg import (hurst, embed_seq, in_range, bin_power, pfd, hfd, hjorth,
+                    spectral_entropy, svd_entropy, fisher_info, ap_entropy,
+                    samp_entropy, dfa)
 
 __all__ = ['psd', 'lowpass', 'highpass', 'bandpass', 'notch', 'hilbert',
            'hilbert_amplitude', 'hilbert_phase', 'cwt', 'cwt_distributed',
