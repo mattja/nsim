246ecc3e387153bde5a54c71d217ce5f454d2d0f
Matthew Aburn <mattja6@gmail.com>
2016-01-23 15:21:45 +1000

    added example network models

---
+++ examples/example_kuramoto.py
@@ -0,0 +1,40 @@
+"""Kuramoto network: N simple phase oscillators coupled together.
+
+This example also shows how a model parameter can be a probability distribution
+instead of a number. Then each time an instance is created from the model class
+recipe it will be given a specific value drawn from the distribution. 
+"""
+
+import nsim
+import numpy as np
+from scipy import stats
+
+class PhaseOscillator(nsim.ODEModel):
+    """A simple oscillator. It just has a phase y that increases at a
+    constant angular velocity omega.  dy/dt = omega """
+    # each oscillator's natural frequency is drawn from a distribution:
+    omega = stats.norm(20.0, 0.1)
+    # each oscillator's initial phase is chosen at random:
+    y0 = stats.uniform(-np.pi, scale=2*np.pi)
+
+    def f(self, y, t):
+        return self.omega
+
+    def coupling(self, source_o, target_y, weight):
+        return weight * np.sin(source_o[0] - target_y[0])
+
+
+# now couple together N of those oscillators:
+N = 100
+nodes = [PhaseOscillator() for i in range(N)]
+
+# network is all-to-all weak coupling:
+coupling_strength = 0.24
+network = (np.ones((N, N)) - np.identity(N)) * coupling_strength / N
+
+model = nsim.NetworkModel(nodes, network)
+sim = nsim.NetworkSim(model, T=60.0)
+ts = sim.timeseries.mod2pi() # interpret y as a phase variable (mod 2pi)
+ts[:,:,53:58].plot(title='viewing five of the %d oscillators' % N)
+ts.order_param().plot(title='phase synchronization order parameter')
+ts.phase_histogram()
---
+++ examples/example_network.py
@@ -0,0 +1,33 @@
+"""A simple network model.
+
+This example couples together 4 Jansen-Rit models using a 
+directed connectivity graph with different connection strengths.
+
+This example also shows how a model can define its own custom coupling function
+
+(In this particular model, for each connection from a source to a target the
+target model's input variable v[5] is driven by a sigmoid function S of the
+difference between the source model's two outputs: S(v[1] - v[2]), weighted by
+the connection strength. This coupling is defined in nsim.models.JansenRit.)
+"""
+
+import nsim
+import numpy as np
+
+nodes = [nsim.models.JansenRit() for i in range(4)] # four nodes in the network
+
+network = np.array([[  0,   0.2, 0.6,   0],  # weighted directed graph
+                    [0.2,     0,   0,   0],
+                    [0.2,     0,   0, 0.3],
+                    [  0,   0.2, 0.3,   0]])
+
+model = nsim.NetworkModel(nodes, network)
+sim = nsim.NetworkSim(model, T=60.0)
+sim.timeseries.plot(title='time series at each node, with network coupling')
+
+
+# try the same simulation without any coupling between them:
+network2 = np.zeros((4, 4))
+model2 = nsim.NetworkModel(nodes, network2)
+sim2 = nsim.NetworkSim(model2, T=60.0)
+sim2.timeseries.plot()
